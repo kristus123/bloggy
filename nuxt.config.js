d4087657b25e3a838e55ecea5b0a9fb2fb8a4d69
kristus123 <krispetter@gmail.com>
2021-09-08 20:23:31 +0200

    x

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/<repository-name>/'
-  },
-
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - bloggy',
