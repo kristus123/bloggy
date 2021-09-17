316b46a918973d7d559265ffd53cd46f55c55c8b
kristus123 <krispetter@gmail.com>
2021-09-17 21:06:17 +0200

    c

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueGtag from "vue-gtag";
 
-Vue.use(VueGtag, {
-  config: { id: "UA-207866487-1" } // 2800628753
-});
+export default ({ app }) => {
+  Vue.use(VueGtag, {
+    config: { id: 'UA-207866487-1' },
+  }, app.router);
+}
