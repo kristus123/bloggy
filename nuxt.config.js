f9b0e0f9e1d7eb55d4e3d205138f3f82f373a25a
kristian <kristian.solbakken@dnb.no>
2021-09-17 12:34:31 +0200

    x

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
+  ssr: true,
 
   generate: {
     fallback: true
