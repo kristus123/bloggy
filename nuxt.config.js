d87c786acef498ea4e3536b109c3a2de1c8979b1
kristus123 <krispetter@gmail.com>
2021-09-24 18:42:58 +0200

    c

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-gtag',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- plugins/vue-gtag.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import VueGtag from "vue-gtag";
-
-export default ({ app }) => {
-  Vue.use(VueGtag, {
-    config: { id: 'UA-207866487-1' },
-  }, app.router);
-}
