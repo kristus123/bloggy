f83ffcaea9010ffde1cbf0937c0f4df83ba94ed3
Your Name <you@example.com>
2024-08-05 11:10:11 +0200

    x

--- pages/content/index.vue
+++ pages/content/index.vue
@@ -7,15 +7,7 @@ export default {
   data() {
     return {
       selectedCategory: {name:"Select a topic", path: null},
-      categories: [
-        {name:"checklists", path: "blog/checklists"},
-        {name:"engineering", path: "blog/engineering"},
-        {name:"philosophy", path: "blog/philosophy"},
-        {name:"tutorials", path: "blog/tutorials"},
-        {name:"trading", path: "blog/trading"},
-        {name:"music", path: "blog/music"},
-        {name:"life", path: "blog/life"},
-      ],
+      categories: REPLACE_WITH_FOLDER_INFO,
     }
   },
 
