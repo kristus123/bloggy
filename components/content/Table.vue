8cf7fb47cc61d9a522cc381cd62b3439c30e7b88
Your Name <you@example.com>
2024-08-07 19:18:51 +0200

    x

---
+++ components/content/Table.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['headers', 'rows']
+}
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+table, th, td {
+  border: 1px solid black;
+}
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+</style>
+
--- content/blog/test/hei.md
+++ content/blog/test/hei.md
@@ -5,3 +5,8 @@ description: swag
 
 - [kilde](url.com)
 - tekst
+
+
+<content-table :headers="['tiss', 'tass']" :rows="[
+	['blisj', 'blasj'],
+]"></content-table>
